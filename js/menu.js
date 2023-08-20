49299997788e0ec5ea6d00be6da5c33766db66ff
MelJhenn <78235574+MelJhenn@users.noreply.github.com>
2023-08-19 20:17:56 -0400

    sev stuff

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+$(function () {
+    $("#mimenu").html(`<ul class="u-nav u-popupmenu-items u-unstyled u-nav-2">
+        <li class="u-nav-item"><a class="u-button-style u-nav-link active"
+        href="index.html">Inicio</a>
+        </li>
+        <li class="u-nav-item">Carreras</li>
+        </ul>
+        <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
+        <li class="u-nav-item"><a class="u-button-style u-nav-link active"
+        href="https://website1649510.nicepage.io/">Carrera 1</a>
+        </li>
+        <li class="u-nav-item"><a class="u-button-style u-nav-link active"
+        href="https://website1649510.nicepage.io/">Carrera 2</a>
+        </li>
+        </ul>
+        `);
+});
+
+
+
+
+
+
+

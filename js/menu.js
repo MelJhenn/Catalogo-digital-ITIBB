b8fecacabc7170f1f8959eadc7318a7f64c2e911
MelJhenn <78235574+MelJhenn@users.noreply.github.com>
2023-09-14 12:38:45 -0400

    sev stuff

--- js/menu.js
+++ js/menu.js
@@ -7,10 +7,28 @@ $(function () {
         </ul>
         <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="https://website1649510.nicepage.io/">Carrera 1</a>
+        href="https://website1649510.nicepage.io/">EMD</a>
         </li>
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="https://website1649510.nicepage.io/">Carrera 2</a>
+        href="https://website1649510.nicepage.io/">CCC</a>
+        </li>
+        <li class="u-nav-item"><a class="u-button-style u-nav-link active"
+        href="https://website1649510.nicepage.io/">EMC</a>
+        </li>
+        <li class="u-nav-item"><a class="u-button-style u-nav-link active"
+        href="https://website1649510.nicepage.io/">IIN</a>
+        </li>
+        <li class="u-nav-item"><a class="u-button-style u-nav-link active"
+        href="https://website1649510.nicepage.io/">GPP</a>
+        </li>
+        <li class="u-nav-item"><a class="u-button-style u-nav-link active"
+        href="https://website1649510.nicepage.io/">ITC</a>
+        </li>
+        <li class="u-nav-item"><a class="u-button-style u-nav-link active"
+        href="https://website1649510.nicepage.io/">MSF</a>
+        </li>
+        <li class="u-nav-item"><a class="u-button-style u-nav-link active"
+        href="index.html">Contactanos</a>
         </li>
         </ul>
         `);

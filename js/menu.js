4e5ee50f6167d61aa047b8b02d82f08332a77dc5
MelJhenn <78235574+MelJhenn@users.noreply.github.com>
2023-09-15 13:29:05 -0400

    sev stufff

--- js/menu.js
+++ js/menu.js
@@ -7,25 +7,25 @@ $(function () {
         </ul>
         <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="https://website1649510.nicepage.io/">EMD</a>
+        href="https://website1649510.nicepage.io/" title="Electromedicina">EMD</a>
         </li>
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="https://website1649510.nicepage.io/">CCC</a>
+        href="https://website1649510.nicepage.io/" title="Construcción Civil">CCC</a>
         </li>
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="https://website1649510.nicepage.io/">EMC</a>
+        href="https://website1649510.nicepage.io/" title="Electromecánica">EMC</a>
         </li>
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="https://website1649510.nicepage.io/">IIN</a>
+        href="https://website1649510.nicepage.io/" title="Informática Industrial">IIN</a>
         </li>
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="https://website1649510.nicepage.io/">GPP</a>
+        href="https://website1649510.nicepage.io/" title="Gas, Petróleo y Procesos">GPP</a>
         </li>
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="https://website1649510.nicepage.io/">ITC</a>
+        href="https://website1649510.nicepage.io/" title="Industria Textil y Confección">ITC</a>
         </li>
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="https://website1649510.nicepage.io/">MSF</a>
+        href="https://website1649510.nicepage.io/" title="Metalurgia, Siderurgia y Fundición">MSF</a>
         </li>
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
         href="index.html">Contactanos</a>
